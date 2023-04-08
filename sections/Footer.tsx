c424b5858937bd6848f25b77acac4bf8b00cd180
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2023-04-07 21:47:14 -0300

    Remove section

---
+++ sections/Footer.tsx
@@ -0,0 +1,18 @@
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+
+export interface Props {
+  textLeft?: string;
+  textRight?: string;
+}
+
+export default function Footer({
+    textLeft,
+    textRight,
+}: Props) {
+  return (
+    <div class="flex items-center justify-between px-8 mb-8 mt-[120px]">
+        <div class="block">{textLeft}</div>
+        <div class="block">{textRight}</div>
+    </div>
+  );
+}
---
+++ sections/Header.tsx
@@ -0,0 +1,39 @@
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+
+export interface Link {
+  label: string;
+  href: string;
+}
+
+export interface Props {
+  avatar: LiveImage;
+  links?: Link[];
+}
+
+export default function Header({
+  avatar,
+  links,
+}: Props) {
+  return (
+    <div class="flex items-center justify-between px-8 mt-3">
+        <div class="flex gap-2 items-center">
+            <img
+                src={avatar}
+                height="40"
+                width="40"
+                alt="Davi Shamá"
+                decoding="async"
+                loading="lazy"
+            />
+            Davi Shamá
+        </div>
+        <div class="flex gap-3">
+            {
+                links?.map(({ label, href }) => (
+                    <a class="block py-2 px-4 hover:underline" href={href}>{label}</a>
+                ))
+            }
+        </div>
+    </div>
+  );
+}
