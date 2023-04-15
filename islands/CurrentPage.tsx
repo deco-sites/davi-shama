4c47baabfd75915c1d10c3f2d11e8d2007a4421c
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2023-04-15 15:09:56 -0300

    Fix font weight

--- islands/CurrentPage.tsx
+++ islands/CurrentPage.tsx
@@ -7,7 +7,6 @@ export default function CurrentPage({
 }: Props) {
     let next = pages[0]
     pages?.forEach((page, i) => {
-        console.log('page', page, i)
         if (page === window?.location?.pathname && i < pages.length - 1) next = pages[i + 1]
     })
   return (
@@ -19,7 +18,7 @@ export default function CurrentPage({
             {
                 <a
                     href={next}
-                    class="inline-block bg-black text-white px-7 py-4"
+                    class="inline-block bg-black text-white px-7 py-4 font-medium"
                 >
                     Next Project
                 </a>
