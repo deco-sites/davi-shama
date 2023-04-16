edaa2c0d2ce973be648914916947765c2a8fb283
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2023-04-16 19:51:13 -0300

    Fix lg container

--- sections/Banner.tsx
+++ sections/Banner.tsx
@@ -28,14 +28,13 @@ export default function Projects({
         banners.map(banner => (
           <div
             class={`flex items-center justify-center
-            ${banner.bgColor && `bg-[${banner.bgColor}]`}
+            ${banner.bgColor ? `bg-[${banner.bgColor}]` : ''}
             ${banner.expand ? `flex-auto` : `flex-none`}
             ${`pt-[${banner.padding?.top ? banner.padding.top : '0'}]`}
             ${`pb-[${banner.padding?.bottom ? banner.padding.bottom : '0'}]`}
           `}>
             <Picture>
               <Source
-                media="(min-width: 768px)"
                 src={banner.image}
               />
               <img
