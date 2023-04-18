805f3fe6e62af52406a921f0a0a4ce78a80cba07
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2023-04-17 22:32:24 -0300

    Teste animation

--- islands/Animate.tsx
+++ islands/Animate.tsx
@@ -4,6 +4,7 @@ export type Props = {
     id: array,
     add?: array,
     remove?: array,
+    event?: 'pageLoad' | 'elementVisible'
 }
 
 class Animate extends Component<Props> {
@@ -13,16 +14,41 @@ class Animate extends Component<Props> {
   }
 
   componentDidMount() {
-    this.props.id.forEach(e => {
-      this.props.remove.forEach(r => {
-        document?.getElementById(e).classList.remove(r)
-      });
-      if (this.props.add) {
-        this.props.add.forEach(a => {
-          document?.getElementById(e).classList.add(a)
+    if (this.props.event !== 'elementVisible') {
+      this.props.id.forEach(e => {
+        this.props.remove.forEach(r => {
+          document?.getElementById(e).classList.remove(r)
         });
-      }
-    });
+        if (this.props.add) {
+          this.props.add.forEach(a => {
+            document?.getElementById(e).classList.add(a)
+          });
+        }
+      });
+    } else {
+      let isVisible = false;
+      const element = document?.getElementById(this.props.id[0]);
+      const options = {
+        root: document,
+        rootMargin: '0px',
+        threshold: 0.6
+      };
+      const observer = new IntersectionObserver((entries, options) => {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio > 0 && !isVisible) {
+            isVisible = !isVisible;
+            this.props.remove.forEach(r => {
+              element.classList.remove(r)
+              console.log("visível", element);
+            });
+            this.props.add.forEach(a => {
+              element.classList.add(a)
+            });
+          }
+        });
+      }, options);
+      observer.observe(element);
+    }
   }
 
   componentDidUpdate() {
